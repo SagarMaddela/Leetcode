75b1e6cc38d3db7f63e37bcd66d87522ca8180d4
SagarMaddela <mrsunday0708.23@gmail.com>
2025-02-05 21:36:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (85.97%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,13 +1,31 @@
 class Solution {
- public:
-  // Similar to 859. Buddy Strings
-  bool areAlmostEqual(string s1, string s2) {
-    vector<int> diffIndices;
-    for (int i = 0; i < s1.length(); ++i)
-      if (s1[i] != s2[i])
-        diffIndices.push_back(i);
-    return diffIndices.empty() || (diffIndices.size() == 2 &&
-                                   s1[diffIndices[0]] == s2[diffIndices[1]] &&
-                                   s1[diffIndices[1]] == s2[diffIndices[0]]);
-  }
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        int length = s1.length();
+        int mismatched_count = 0;
+        char mismatched_letters[2];
+        for(int i = 0;i < length;i++){
+            if(s1[i] != s2[i]) {
+                mismatched_count++;
+                switch (mismatched_count) {
+                    case 1:
+                    mismatched_letters[0] = s1[i];
+                    mismatched_letters[1] = s2[i];
+                    break;
+                    case 2:
+                    if((mismatched_letters[0] != s2[i]) || (mismatched_letters[1] != s1[i]))
+                    return false;
+                    break;
+                    case 3:
+                    return false;
+                    break;
+                }
+            }
+        }
+
+        if(mismatched_count == 1)
+        return false;
+        else
+        return true;
+    }
 };
