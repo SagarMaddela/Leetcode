c2c925af31e2b466d3ca205f960a9df927c24b68
Venkata Sagar <mrsunday0708.23@gmail.com>
2025-04-01 19:51:21 +0530

    Time: 4 ms (75.29%) | Memory: 119.9 MB (54.42%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.cpp
@@ -3,15 +3,12 @@ public:
     long long mostPoints(vector<vector<int>>& questions) {
         int rows = questions.size();
         vector<long long> dp(rows, 0);
-
-        // Start from the last question
         dp[rows - 1] = questions[rows - 1][0];
 
         for (int i = rows - 2; i >= 0; i--) {
-            long long skip = dp[i + 1];  // If we skip this question
-            long long take = questions[i][0];  // If we take this question
+            long long skip = dp[i + 1];  
+            long long take = questions[i][0];  
 
-            // Ensure we don't go out of bounds
             if (i + 1 + questions[i][1] < rows) {
                 take += dp[i + 1 + questions[i][1]];
             }
@@ -19,6 +16,6 @@ public:
             dp[i] = std::max(skip, take);
         }
 
-        return dp[0];  // Maximum points from the first question
+        return dp[0]; 
     }
 };
