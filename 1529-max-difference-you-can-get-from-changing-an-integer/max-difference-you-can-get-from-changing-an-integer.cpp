f3493edb92e45b3e4ecf6f8c2b9146c81166c121
Venkata Sagar <mrsunday0708.23@gmail.com>
2025-06-15 15:04:20 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (43.13%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
@@ -5,31 +5,21 @@ public:
         std::string maxi = num_str;
         std::string mini = num_str;
         int size = maxi.length();
-        
+
         int j = 0;
         while(num_str[j] == '9' && j < size) j++;
         char target = num_str[j];
-        //cout << "Max target is :" << target  << " & j is "<< j << endl;
 
-        for(int i = 0;i < size;i++){
-            if(num_str[i] == target) maxi[i] = '9';
-        }
+        for(int i = 0;i < size;i++) if(num_str[i] == target) maxi[i] = '9';
 
         j = 0;
         while((num_str[j] == '1'|| num_str[j] == '0') && j < size) j++;
         target = num_str[j];
-        char pivot = '1';
-        if(j) pivot = '0'; 
-        //cout << "Mini target is :" << target << endl;
-    
-        for(int i = 0;i < size;i++){
-            if(num_str[i] == target){
-                mini[i] = pivot;
-            }
-           // cout << "Mini at " << i << " is " << mini[i] << endl;
-        }
 
-       // cout << stoi(maxi) << " - " << stoi(mini) << endl; 
+        char pivot = '1';
+        if(j) pivot = '0';
+         
+        for(int i = 0;i < size;i++) if(num_str[i] == target) mini[i] = pivot;
         return stoi(maxi) - stoi(mini);
     }
 };
