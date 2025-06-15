e0c3bd7c9101f24e98a0227b6b774d73cefc1e3d
Venkata Sagar <mrsunday0708.23@gmail.com>
2025-06-15 15:01:23 +0530

    Time: 13 ms (4.27%) | Memory: 8.2 MB (43.13%) - LeetSync

---
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int maxDiff(int num) {
+        std::string num_str = to_string(num);
+        std::string maxi = num_str;
+        std::string mini = num_str;
+        int size = maxi.length();
+        for(int i = 0 ; i < size;i++){
+            cout << mini[i] << " ";
+        }
+        cout << endl;
+        cout << "size is :" << size << endl;
+
+        int j = 0;
+        while(num_str[j] == '9' && j < size) j++;
+        char target = num_str[j];
+        cout << "Max target is :" << target  << " & j is "<< j << endl;
+
+        for(int i = 0;i < size;i++){
+            if(num_str[i] == target) maxi[i] = '9';
+        }
+
+        j = 0;
+        while((num_str[j] == '1'|| num_str[j] == '0') && j < size) j++;
+        target = num_str[j];
+        char pivot = '1';
+        if(j) pivot = '0'; 
+        cout << "Mini target is :" << target << endl;
+    
+        for(int i = 0;i < size;i++){
+            if(num_str[i] == target){
+                mini[i] = pivot;
+            }
+            cout << "Mini at " << i << " is " << mini[i] << endl;
+        }
+
+        // cout << "Sum is : " << sum << endl;
+        // if(sum == 0){
+        //     for(int index : indices) mini[index] = '1';
+        // }
+
+        cout << stoi(maxi) << " - " << stoi(mini) << endl; 
+        return stoi(maxi) - stoi(mini);
+    }
+};
