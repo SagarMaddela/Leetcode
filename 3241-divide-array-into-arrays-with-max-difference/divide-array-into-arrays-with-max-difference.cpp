30b56d26bb296a7bd389e57cdea1666eb8db5249
Venkata Sagar <mrsunday0708.23@gmail.com>
2025-06-18 17:35:02 +0530

    Time: 52 ms (44.26%) | Memory: 120 MB (81.34%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -6,8 +6,6 @@ public:
         std::vector<vector<int>> result;
         while(i < n){
             if(nums[i+2] - nums[i] <= k){
-                //std::cout << "Group is valid (max - min <= " << k << "): ";
-               // std::cout << "[" << nums[i] << ", " << nums[i + 1] << ", " << nums[i + 2] << "]" << std::endl;
                 result.push_back({nums[i], nums[i+1], nums[i+2]});
                 i += 3;
             } else {
